5206696ce929c834445a31fd055e3c758da00de4
Max <max0n@me.com>
2021-01-31 19:46:54 +0300

    Setup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import ChatView from './components/ChatView';
+import { useForm } from "react-hook-form";
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import MessageIcon from '@material-ui/icons/Message';
 
 function App() {
+  const { handleSubmit } = useForm();
+  const [messageValue, setMessageValue] = useState<string | null>('');
+
+  const onSubmit = () => {
+    console.log(messageValue);
+    setMessageValue('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatView />
+      <p>{messageValue}</p>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+        <TextField
+          name="message"
+          type="text"
+          value={messageValue}
+          onChange={event => setMessageValue(event.target.value)}
+          label="Write message here..." />
+        <IconButton color="primary" type="submit">
+          <MessageIcon />
+        </IconButton>
+      </form>
     </div>
   );
 }
---
+++ src/components/ChatView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatView() {
+  return (
+    <section className="ChatView">
+      hello!
+    </section>
+  );
+}
+
+export default ChatView;
