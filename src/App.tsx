cc56e0aff4e7478f86694610af5bacb46fc46bf2
Max <max0n@me.com>
2021-01-31 21:14:26 +0300

    Add show chat messages

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,46 @@ import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import MessageIcon from '@material-ui/icons/Message';
 
-function App() {
-  const { handleSubmit } = useForm();
-  const [messageValue, setMessageValue] = useState<string | null>('');
+type ChatMessage = {
+  id: string,
+  isOwner: boolean,
+  message: string,
+  reply?: ChatMessage
+}
 
-  const onSubmit = () => {
-    console.log(messageValue);
-    setMessageValue('');
+type ChatForm = {
+  message: string
+}
+
+function App() {
+  const { register, handleSubmit, setValue } = useForm();
+  const [chatList, setChatList] = useState<ChatMessage[]>([]);
+
+  const onSubmit = (from: ChatForm) => {
+    console.log('34', from);
+
+    const chatItem: ChatMessage = {
+      id: String(Math.random()),
+      isOwner: true,
+      message: from.message
+    }
+
+    setChatList([...chatList, chatItem]);
+    setValue("message", "");
   };
+
+  const replyUpdated = (value: any) => {
+    console.log('raplyHandler', value)
+  }
 
   return (
     <div className="App">
-      <ChatView />
-      <p>{messageValue}</p>
+      <ChatView chatList={chatList} replyUpdated={replyUpdated} />
       <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
         <TextField
           name="message"
           type="text"
-          value={messageValue}
-          onChange={event => setMessageValue(event.target.value)}
+          inputRef={register}
           label="Write message here..." />
         <IconButton color="primary" type="submit">
           <MessageIcon />
--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function ChatView() {
+function ChatView({ chatList, replyUpdated }: any) {
+
+  const [replyMessage, setReplyMessage] = useState<any | null>(null);
+
+  useEffect(() => {
+    console.log('$', chatList);
+    setReplyMessage(null);
+  }, [chatList]);
+
+  const onClickHandler = (chatItem: any) => {
+    if (replyMessage && replyMessage === chatItem) {
+      setReplyMessage(null);
+      replyUpdated(null);
+    } else {
+      setReplyMessage(chatItem);
+      replyUpdated(chatItem);
+    }
+  };
+
   return (
     <section className="ChatView">
-      hello!
+      <ol>
+        {chatList.map((chatItem: any) => (
+          <li
+            key={chatItem.id}
+            onClick={() => { onClickHandler(chatItem) }}
+          >
+            {chatItem.message}
+          </li>
+        ))}
+      </ol>
     </section>
   );
 }
